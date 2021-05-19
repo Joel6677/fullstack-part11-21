98164657f247a9d2b66f34ee9a56b121da87310f
Joel6677 <joel.heusala@gmail.com>
2021-05-19 13:55:02 +0300

    cypress fix

---
+++ cypress/integration/e2e.js
@@ -0,0 +1,13 @@
+describe('Redux-anecdotes', function () {
+    it('front page can be opened', function () {
+      cy.visit('http://localhost:3001')
+      cy.contains('Anecdotes')
+      cy.contains('create new')
+    })
+        
+    it('can add anecdote', function () {
+      cy.get('#create-anecdote').type('testi')
+      cy.get('#add-button').click()
+      cy.contains('you created \'testi\'')
+    })
+  })
