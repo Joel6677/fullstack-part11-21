76998e483fb9b6b98d5393169935f93a5fbf7ead
Joel6677 <joel.heusala@gmail.com>
2021-05-19 13:37:54 +0300

    fix cypress test

--- redux-anecdotes/cypress/integration/e2e.js
+++ redux-anecdotes/cypress/integration/e2e.js
@@ -1,6 +1,6 @@
 describe('Redux-anecdotes', function () {
   it('front page can be opened', function () {
-    cy.visit('http://localhost:3000')
+    cy.visit('http://localhost:3001')
     cy.contains('Anecdotes')
     cy.contains('create new')
   })
